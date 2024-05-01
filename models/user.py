4bad00ab57c276ef3bdc097235160b0f9dbd1dc5
eufot60 <eufot60@gmail.com>
2024-05-01 13:30:07 -0700

    a

--- models/user.py
+++ models/user.py
@@ -2,25 +2,17 @@
 """This module defines a class User"""
 """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
-    '''
-        Definition of the User class
-    '''
+    """This class defines a user by various attributes"""
     __tablename__ = "users"
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", passive_deletes=True, backref="user")
-        reviews = relationship("Review", passive_deletes=True, backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade='delete')
+    reviews = relationship('Review', backref='user', cascade='delete')
